89a2f97f5386df80c9dd0581e7e2e898b6ec5c55
dmpe <cincenko@outlook.com>
2020-07-08 23:23:24 +0200

    change path

--- finance/settings.py
+++ finance/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'finance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'main/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

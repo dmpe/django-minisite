c5c87bef1bbb50db387afea07fde50a8fd4ece36
dmpe <cincenko@outlook.com>
2020-08-09 18:48:57 +0200

    reg

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import Firm_Recommendation, Firm_User
 
 # Register your models here.
+
+admin.site.register(Firm_Recommendation)
+admin.site.register(Firm_User)

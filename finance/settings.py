f1f59fa899eade7d488b16cbf082a2dfd71f3de6
dmpe <cincenko@outlook.com>
2020-08-09 15:25:27 +0200

    add rest api and swagger

--- finance/settings.py
+++ finance/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'drf_yasg',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [

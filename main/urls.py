a2afbdff152d7c27a4199834013643e498bcec44
dmpe <cincenko@outlook.com>
2020-08-09 21:05:16 +0200

    test

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 import rest_framework
 from rest_framework import routers
 from rest_framework.authtoken import views
-from main.views import *
 
 from . import views
 
@@ -14,5 +13,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("api-token-auth/", views.obtain_auth_token),
+    path("api-token-auth/", rest_framework.authtoken.views.obtain_auth_token),
 ]

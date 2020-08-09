607af72c34a1d9e136009fcf00a8a9b51d639cec
dmpe <cincenko@outlook.com>
2020-08-09 20:25:00 +0200

    all

--- main/ser.py
+++ main/ser.py
@@ -10,4 +10,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class FirmRecommendationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Firm_Recommendation
-        fields = ['*']
+        fields = '__all__'

0a6c3c01e6287b26bbef24de43985d94c82b31ea
dmpe <cincenko@outlook.com>
2020-08-15 14:10:56 +0200

    test

--- main/form.py
+++ main/form.py
@@ -1,6 +1,8 @@
 from django import forms
+from rest_framework import fields
 from . import models
 
 class RecommendationSingleRowForm(forms.ModelForm):
     class Meta:
         model = models.Firm_Recommendation
+        fields = "__all__"

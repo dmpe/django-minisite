26168d3389c3512c8add870499d59b30c725d1b9
dmpe <cincenko@outlook.com>
2020-08-15 11:40:03 +0200

    asd

--- main/urls.py
+++ main/urls.py
@@ -3,14 +3,30 @@ import rest_framework
 from rest_framework import routers
 from rest_framework.authtoken import views
 from rest_framework.utils import urls
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from . import views
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 app_name = "main"
 
 # https://www.django-rest-framework.org/api-guide/routers/
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
+router.register(r"user", views.UserViewSet)
 router.register(r"recommendation", views.RecommendationViewSet)
 
 urlpatterns = [
@@ -18,4 +34,7 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api-token-auth/", rest_framework.authtoken.views.obtain_auth_token),
+    path('^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]

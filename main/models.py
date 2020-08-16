67f06f6a6d14779f8229758d332298a4d713edd8
dmpe <cincenko@outlook.com>
2020-08-16 14:26:49 +0200

    asd

--- main/models.py
+++ main/models.py
@@ -4,13 +4,13 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
-from django_prometheus.models import ExportModelOperationsMixin
+# from django_prometheus.models import ExportModelOperationsMixin
 from rest_framework.authtoken.models import Token
 
-
+ # ExportModelOperationsMixin('firm_recommendation'), see https://github.com/korfuri/django-prometheus/issues/217
 class Firm_Recommendation(
     models.Model
-):  # ExportModelOperationsMixin('firm_recommendation'), see https://github.com/korfuri/django-prometheus/issues/217
+):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     starting_date = models.DateField()
